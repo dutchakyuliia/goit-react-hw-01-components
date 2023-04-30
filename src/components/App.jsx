48b0b4147f3209afbd3c32ed53a1f58683232883
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-04-30 20:42:07 +0200

    fix bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
-import Stats from './Profile/Stats';
 import Statistics from './Statistics/Statistics';
-import data from './Statistics/data.json';
 import friends from './FriendListItem/friends.json';
 import FriendList from './FriendListItem/FriendList';
 import TransactionTable from './TransactionHistory/TransactionTable';
-import transactions from './TransactionHistory/transactions.json';
 export const App = () => {
   return (
     <div>
@@ -18,7 +15,7 @@ export const App = () => {
         stats={user.stats}
       />
       
-      <Statistics title stats />
+      <Statistics />
       <FriendList friends={friends} />;
       <TransactionTable />;
     </div>
